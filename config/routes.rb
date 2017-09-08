a06a2c3e211851a7eff639e480f3354850f04aad
Thiago Sawada <tms1_tms2@hotmail.com>
2017-09-07 21:10:17 -0300

    Config routes file for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Namespace cria um prefixo na URL (www.site.com/api)
+
+  # Configurações defaults:
+  # Tipos de midia - todas as requisições aos controllers dentro do namespace api serão no formato json
+
+  # Configurações constraints
+  # Para acessar os controles é preciso passar o subdomínio api. Com isso, a URL passa a ser api.site.com/api/
+
+  # O path: "/" elimina o caminho o /api. Agora fica api.site.com
+
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+
+  end
 end
