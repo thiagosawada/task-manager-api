3e7f53f82591ffb65f5110c39660adbecd6d3343
Thiago Sawada <tms1_tms2@hotmail.com>
2017-09-08 11:42:19 -0300

    Adding devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_version_constraint' # Tá na pasta lib
 
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Namespace cria um prefixo na URL (www.site.com/api)
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+# Arquivo pra trabalhar com testes
+
+FactoryGirl.define do
+  factory :user do
+
+  end
+end
